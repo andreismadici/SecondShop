a6c37e70141939fbe509775db19b02fe12daae8f
Andrei Smadici <Andrei.Smadici@yahoo.com>
2021-10-14 14:16:59 +0300

    Init Commit

---
+++ src/main/java/com/example/SecondShop/SecondShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecondShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SecondShop/SecondShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecondShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
